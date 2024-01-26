8ca53a8939d9ee5ef7f6414671e770e671fcc2d4
Marvin Dalheimer <Marvin.Dalheimer@1und1.de>
2024-01-26 20:54:22 +0100

    add crowd sitting on tables
    It is configurable how many people are spawned

---
+++ godot/scenes/crowd/crowd.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export_range(1, 16, 1)
+var max_persons = 16
+
+func _ready():
+	var counter = 0
+	var person = preload("res://scenes/crowd/person.tscn")
+	for seat in $Seats.get_children():
+		seat.add_child(person.instantiate())
+		
+		counter += 1
+		if counter == max_persons:
+			break
