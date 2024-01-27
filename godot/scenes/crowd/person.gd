4df12455abb9854357d02604ca626f0660e9d80f
Mikhail Aristov <12281487+MikhailAristov@users.noreply.github.com>
2024-01-27 15:40:59 +0100

    +random bodies

--- godot/scenes/crowd/person.gd
+++ godot/scenes/crowd/person.gd
@@ -2,9 +2,12 @@ class_name AudienceMember
 extends Node2D
 
 @export var mood : float = 0.
+@export_enum("blue", "green", "red") var color
 
 @export var head : Node2D
 @export var face : Node2D
+@export var body : Node2D
+@export var body_shape : Node2D
 @export var profile : AudienceProfile
 
 @export_enum("angry", "neutral", "happy", "laugh") var expression
@@ -17,6 +20,27 @@ var known_faces : Array[String] = [
 	# Add more scenes as needed
 ]
 
+var known_bodies = {
+	"blue" : [
+		"res://scenes/bodies/body_blue_1.tscn",
+		"res://scenes/bodies/body_blue_2.tscn",
+		"res://scenes/bodies/body_blue_3.tscn",
+		# Add more scenes as needed
+	],
+	"green" : [
+		"res://scenes/bodies/body_green_1.tscn",
+		"res://scenes/bodies/body_green_2.tscn",
+		"res://scenes/bodies/body_green_3.tscn",
+		# Add more scenes as needed
+	],
+	"red" : [
+		"res://scenes/bodies/body_red_1.tscn",
+		"res://scenes/bodies/body_red_2.tscn",
+		"res://scenes/bodies/body_red_3.tscn",
+		# Add more scenes as needed
+	],
+}
+
 const laughter_duration : float = 2. # seconds
 const laughter_bobs : int = 4
 var laughter_left : float = 0.
@@ -26,6 +50,9 @@ func _ready():
 	laughter_left = 0.
 	mood = randf_range(profile.lashout_threshold, profile.happy_threshold)
 	update_expression()
+	
+	if body_shape == null:
+		set_random_body(color)
 	
 	if face == null:
 		set_random_face()
@@ -69,6 +96,16 @@ func update_expression():
 		expression = "angry"
 	else:
 		expression = "neutral"
+		
+func set_random_body(for_color):
+	if str(for_color) not in known_bodies:
+		var keys = known_bodies.keys()
+		for_color = keys[randi() % keys.size()]
+		print(keys.size())
+	var body_path = known_bodies[for_color][randi() % known_bodies[for_color].size()]
+	var body_res = load(body_path)
+	body_shape = body_res.instantiate()
+	body.add_child(body_shape)
 
 func set_random_face():
 	var face_res = load(known_faces[randi() % known_faces.size()])
