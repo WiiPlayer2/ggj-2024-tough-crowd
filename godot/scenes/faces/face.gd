91281d9cb7718298f83c6536342fa7ca7a17ab53
Mikhail Aristov <12281487+MikhailAristov@users.noreply.github.com>
2024-01-27 12:40:54 +0100

    make it work

--- godot/scenes/faces/face.gd
+++ godot/scenes/faces/face.gd
@@ -2,19 +2,8 @@ extends Node2D
 
 @export_enum("angry", "neutral", "happy", "laugh") var expression
 
-@export var sprite_laugh : Sprite2D
-@export var sprite_happy : Sprite2D
-@export var sprite_neutral : Sprite2D
-@export var sprite_unhappy : Sprite2D
-
 var person_controller : AudienceMember
-
-var expression_map = {
-	"angry" : sprite_unhappy,
-	"neutral" : sprite_neutral,
-	"happy" : sprite_happy,
-	"laugh" : sprite_laugh,
-}
+var expression_map = {}
 
 const expression_change_speed : float = 1.5
 const color_transparent : Color = Color(1, 1, 1, 0)
@@ -22,21 +11,24 @@ const color_visible : Color = Color(1, 1, 1, 1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	person_controller = null
+	expression_map = {
+		"angry" : $Unhappy,
+		"neutral" : $Neutral,
+		"happy" : $Happy,
+		"laugh" : $Laugh,
+	}
+	person_controller = get_node("../../../Person")
 	expression = person_controller.expression
 	reset_face()
 	
 func _process(_delta):
 	if expression != person_controller.expression:
 		update_face(person_controller.expression)
-		expression != person_controller.expression
+		expression = person_controller.expression
 
 func reset_face():
 	for expr in expression_map:
-		if expr == expression:
-			expression_map[expr].modulate = color_visible
-		else:
-			expression_map[expr].modulate = color_transparent
+		expression_map[expr].modulate = color_transparent.lerp(color_visible, expr == expression)
 
 func update_face(new_expression: String):
 	var tween = get_tree().create_tween().bind_node(self).set_parallel().set_trans(Tween.TRANS_LINEAR)
