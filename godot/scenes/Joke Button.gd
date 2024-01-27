515c9eefc71d20dcdde50a850bcb01586555150a
Waldemar Tomme <waldetomme@gmail.com>
2024-01-27 11:38:38 +0100

    Add joke buttons

---
+++ godot/scenes/Joke Button.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+signal button_pressed
+
+@export_enum("joke_button_1", "joke_button_2", "joke_button_3") var action: String
+
+var current_joke: Joke
+
+func _init():
+	current_joke = Joke.new(Joke.JokeType.Joke1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed(action):
+		emit_signal("button_pressed", current_joke)
---
+++ godot/types/joke.gd
@@ -0,0 +1,10 @@
+class_name Joke
+
+enum JokeType {
+	Joke1,
+	Joke2,
+	Joke3
+}
+
+func _init(type):
+	self.type = type
