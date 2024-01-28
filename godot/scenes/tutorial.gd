fae416461977d2c2c7aba54f92fb4613bc4545c3
Mikhail Aristov <12281487+MikhailAristov@users.noreply.github.com>
2024-01-28 13:47:32 +0100

    translations: + tutorial + credits

--- godot/scenes/tutorial.gd
+++ godot/scenes/tutorial.gd
@@ -4,18 +4,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 func _input(event):
-    if event.is_action_pressed("interact"):
-        change_scene()
+	if event.is_action_pressed("interact"):
+		change_scene()
 
 func _on_button_pressed():
-    change_scene()
+	change_scene()
 
 func change_scene():
-    get_tree().change_scene_to_packed(next_scene)
+	get_tree().change_scene_to_packed(next_scene)
